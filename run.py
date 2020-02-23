3f97d594146d00f3ffc4c0fdb018e48e6d3b6a25
dkdk <dkdk@dkdkui-MacBookPro.local>
2020-02-23 13:47:12 +0900

    first commit

---
+++ run.py
@@ -0,0 +1,41 @@
+
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import torch.optim as optim
+from torchvision import datasets, transforms
+from torch.autograd import Variable
+from PIL import Image
+from torchvision.transforms import ToTensor
+
+class MnistModel(nn.Module):
+    def __init__(self):
+        super(MnistModel, self).__init__()
+        # input is 28x28
+        # padding=2 for same padding
+        self.conv1 = nn.Conv2d(1, 32, 5, padding=2)
+        # feature map size is 14*14 by pooling
+        # padding=2 for same padding
+        self.conv2 = nn.Conv2d(32, 64, 5, padding=2)
+        # feature map size is 7*7 by pooling
+        self.fc1 = nn.Linear(64*7*7, 1024)
+        self.fc2 = nn.Linear(1024, 10)
+
+    def forward(self, x):
+        x = F.max_pool2d(F.relu(self.conv1(x)), 2)
+        x = F.max_pool2d(F.relu(self.conv2(x)), 2)
+        x = x.view(-1, 64*7*7)   # reshape Variable
+        x = F.relu(self.fc1(x))
+        x = F.dropout(x, training=self.training)
+        x = self.fc2(x)
+        return F.softmax(x, dim=1) # F.log_softmax(x)
+
+model = MnistModel()
+model.load_state_dict(torch.load("./save.pth"))
+
+img = Image.open('3_b.png').convert('L')
+img = ToTensor()(img).unsqueeze(0)
+img = Variable(img)
+
+ret = model(img)
+print(ret.data.max(1))
